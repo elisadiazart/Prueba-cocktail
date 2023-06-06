4bf643a5e95ce1026a1f6665b04b97e09d9baef8
Elisa Diaz <elisadiazartcontact@gmail.com>
2023-06-06 19:12:56 +0000

    06.06.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const navOptions = document.getElementById("navOptions");
+const menuIcon = document.getElementById("menuIcon");
+const hamburguerIcon = document.getElementById("hamburguerIcon");
+const logo = document.getElementById("logo");
+const navContainer = document.getElementById("navContainer");
+
+menuIcon.addEventListener("click", () => {
+  navOptions.classList.toggle("nav__container--close");
+  hamburguerIcon.classList.toggle("nav__menu-icon--close");
+  logo.classList.toggle("nav__logo--close");
+  navContainer.classList.toggle("nav__logo-menu-container--close");
+});
