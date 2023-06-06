9f0233fb16b0a9833fae87dc95c50590b74efdcf
Elisa <elisadiazartcontact@gmail.com>
2023-06-06 23:09:37 +0200

    06.06.23

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,39 @@ const menuIcon = document.getElementById("menuIcon");
 const hamburguerIcon = document.getElementById("hamburguerIcon");
 const logo = document.getElementById("logo");
 const navContainer = document.getElementById("navContainer");
+const bullet1 = document.getElementById("bullet1");
+const bullet2 = document.getElementById("bullet2");
+const bullet3 = document.getElementById("bullet3");
+
+const gallery = document.getElementById("gallery");
 
 menuIcon.addEventListener("click", () => {
   navOptions.classList.toggle("nav__container--close");
   hamburguerIcon.classList.toggle("nav__menu-icon--close");
   logo.classList.toggle("nav__logo--close");
   navContainer.classList.toggle("nav__logo-menu-container--close");
+});
+
+bullet1.addEventListener("click", (e) => {
+  if (e.target.classList.contains("gallery__bullet--active")) return;
+  e.target.classList.add("gallery__bullet--active");
+  bullet2.classList.remove("gallery__bullet--active");
+  bullet3.classList.remove("gallery__bullet--active");
+  gallery.style.transform = `translateX(0%)`;
+});
+
+bullet2.addEventListener("click", (e) => {
+  if (e.target.classList.contains("gallery__bullet--active")) return;
+  e.target.classList.add("gallery__bullet--active");
+  bullet1.classList.remove("gallery__bullet--active");
+  bullet3.classList.remove("gallery__bullet--active");
+  gallery.style.transform = `translateX(-46.5%)`;
+});
+
+bullet3.addEventListener("click", (e) => {
+  if (e.target.classList.contains("gallery__bullet--active")) return;
+  e.target.classList.add("gallery__bullet--active");
+  bullet2.classList.remove("gallery__bullet--active");
+  bullet1.classList.remove("gallery__bullet--active");
+  gallery.style.transform = `translateX(-93%)`;
 });
